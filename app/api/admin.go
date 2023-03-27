9010a36dc3150fcd6a620e73d0556fde6efe7c90
Siren <siren@kernal.eu>
2023-03-27 22:06:09 +0300

    Split api client

--- app/api/admin.go
+++ app/api/admin.go
@@ -2,38 +2,12 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
 
 	"gitlab.com/moneropay/metronero/metronero-backend/app/models"
-
-	"gitlab.com/moneropay/metronero/metronero-frontend/utils/config"
 )
 
-func backendRequest(token, method, endpoint string) ([]byte, error) {
-	endpoint, err := url.JoinPath(config.Backend, endpoint)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest(method, endpoint, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", token))
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	return io.ReadAll(res.Body)
-}
-
 func GetAdminDashboard(token string) (*models.AdminDashboardInfo, error) {
-	resp, err := backendRequest(token, "GET", "/admin")
+	resp, err := backendRequest(token, "GET", "/admin", nil)
 	if err != nil {
 		return nil, err
 	}
