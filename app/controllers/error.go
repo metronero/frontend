8f3083d8ab08d806d36b4cb6ca949009fe9c5f60
Siren <siren@kernal.eu>
2023-04-10 15:28:36 +0300

    Add back to dashboard button functionality on error page

--- app/controllers/error.go
+++ app/controllers/error.go
@@ -1,10 +1,19 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"gitlab.com/metronero/frontend/utils/token"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func serveErrorPage(c *fiber.Ctx, code int, message string) error {
+	dUrl := "/admin/dashboard"
+	if token.GetUsername(c) != "admin" {
+		dUrl = "/merchant/dashboard"
+	}
 	return c.Render("error", fiber.Map{
 		"Code": code,
 		"Message": message,
+		"DashboardUrl": dUrl,
 	})
 }
