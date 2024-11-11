1439b67b74d3af23b0a3864d72e171d640d8d8c4
Siren <siren@kernal.eu>
2024-11-11 06:55:27 +0200

    Merchant Settings

--- src/views/merchant/Settings.vue
+++ src/views/merchant/Settings.vue
@@ -1,44 +1,94 @@
 <script setup lang="js">
 import axios from 'axios';
 import { useToast } from 'primevue/usetoast';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const toast = useToast();
 const { push } = useRouter();
-const instanceHealth = ref(true);
-const instanceVersion = ref('v0.0.0');
-const moneropayVersion = ref('v0.0.0');
-const isLoading = ref(true);
+const completeOn = ref(1);
+const expireAfter = ref('1h');
+const fiatCurrency = ref('EUR');
 
-function getInstance() {
+const completeOnOptions = [
+    { name: '0-conf', code: 0 },
+    { name: '1-conf', code: 1 },
+    { name: '10-conf', code: 10 }
+];
+
+const fiatOptions = [
+    { name: 'Euro', code: 'EUR' },
+    { name: 'US Dollar', code: 'USD' }
+];
+
+const expireOptions = [
+    { name: '5 minutes', code: '5m' },
+    { name: '30 minutes', code: '30m' },
+    { name: '1 hour', code: '1h' },
+    { name: '6 hours', code: '6h' },
+    { name: '12 hours', code: '12h' },
+    { name: '1 day', code: '1d' }
+];
+
+function getSettings() {
     axios
-        .get(import.meta.env.VITE_API_BASE + '/admin/instance', { withCredentials: true })
+        .get(import.meta.env.VITE_API_BASE + '/merchant', { withCredentials: true })
         .then((response) => {
             console.log(response.data);
-            instanceHealth.value = response.data.healthy;
-            instanceVersion.value = response.data.version;
-            moneropayVersion.value = response.data.moneropay;
+            completeOn.value = response.data.complete_on;
+            expireAfter.value = response.data.expire_after;
+            fiatCurrency.value = response.data.fiat_currency;
         })
         .catch((error) => {
             console.log(error);
-            if (error.response) {
-                toast.add({ severity: 'error', summary: 'Failed to fetch instance info', detail: error.response.data.message, life: 3000 });
+            if (error.response?.status === 401) {
+                toast.add({ severity: 'error', summary: 'Not logged in', detail: error.code, life: 3000 });
+                push({ path: '/auth/login' });
             } else {
-                toast.add({ severity: 'error', summary: error.message, detail: error.code, life: 3000 });
+                toast.add({ severity: 'error', summary: 'Failed to fetch merchant settings', detail: error.response.data.message, life: 3000 });
             }
-        })
-        .finally(() => (isLoading.value = false));
+        });
 }
+
+function saveSettings() {
+    axios
+        .post(import.meta.env.VITE_API_BASE + '/merchant', { complete_on: completeOn.value, expire_after: expireAfter.value, fiat_currency: fiatCurrency.value }, { withCredentials: true })
+        .then(() => {
+            toast.add({ severity: 'success', summary: 'Saved merchant settings', life: 3000 });
+        })
+        .catch((error) => {
+            console.log(error);
+            if (error.response?.status === 401) {
+                toast.add({ severity: 'error', summary: 'Not logged in', detail: error.code, life: 3000 });
+                push({ path: '/auth/login' });
+            } else {
+                toast.add({ severity: 'error', summary: 'Failed to save merchant settings', detail: error.response.data.message, life: 3000 });
+            }
+        });
+}
+
+onMounted(() => {
+    getSettings();
+});
 </script>
 
 <template>
     <div>
         <div class="card">
-            <h1 class="font-semibold text-xl mb-5">Account Settings</h1>
-            <p><b>Healthy:</b> {{ instanceHealth }}</p>
-            <p><b>Version:</b> {{ instanceVersion }}</p>
-            <p><b>MoneroPay version:</b> {{ moneropayVersion }}</p>
+            <h1 class="font-semibold text-2xl mb-5">Account Settings</h1>
+            <div class="mb-3">
+                <p class="mb-2"><span class="font-bold">Complete on:</span> After how many confirmations this invoice should be marked as complete.</p>
+                <Select v-model="completeOn" :options="completeOnOptions" optionLabel="name" optionValue="code" checkmark />
+            </div>
+            <div class="mb-3">
+                <p class="mb-2"><span class="font-bold">Expire after:</span> If not paid, after how long should this invoice expire.</p>
+                <Select v-model="expireAfter" :options="expireOptions" optionLabel="name" optionValue="code" checkmark />
+            </div>
+            <div class="mb-3">
+                <p class="mb-2"><span class="font-bold">Default fiat currency:</span> Display values in this currency in PoS mode and payment pages.</p>
+                <Select v-model="fiatCurrency" :options="fiatOptions" optionLabel="name" optionValue="code" checkmark />
+            </div>
+            <Button label="Save" class="mt-5" icon="pi pi-save" @click="saveSettings" />
         </div>
     </div>
 </template>
